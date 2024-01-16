751218fc4a5c2fc76a44509a7bace23ba1d1c15f
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-01-16 19:04:55 +0200

    test

--- models/base_model.py
+++ models/base_model.py
@@ -10,22 +10,19 @@ from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
-
-
-
 
 class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
+    """The BaseModel class from which future classes will be inhurit from """
 
 
     def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+        """ base model """
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
             if kwargs.get("created_at", None) and type(self.created_at) is str:
+                time = "%Y-%m-%dT%H:%M:%S.%f"
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
             else:
                 self.created_at = datetime.utcnow()
@@ -41,19 +38,20 @@ class BaseModel:
             self.updated_at = self.created_at
 
     def __str__(self):
-        """String representation of the BaseModel class"""
+        """ BaseModel String representation """
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
                                          self.__dict__)
 
     def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
+        """Saves the obj with the current time """
         self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
+        """returns a dictionary representation """
         new_dict = self.__dict__.copy()
+        time = "%Y-%m-%dT%H:%M:%S.%f"
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
