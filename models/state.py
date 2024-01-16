e5b8a938b0b4642d2d6676c7a048b7ff9eb24c19
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-01-16 14:46:06 +0200

    modified create and print all

--- models/state.py
+++ models/state.py
@@ -6,3 +6,7 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ Initializes State object """
+        super().__init__(*args, **kwargs)
