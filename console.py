32d146eb1842dcf6e88be563e9028713c4195760
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-01-17 13:26:12 +0200

    Added setup_mysql_dev.sql

--- console.py
+++ console.py
@@ -113,9 +113,9 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def _key_value_parser(self, args):
-        """creates a dictionary from a list of strings"""
-        new_dict = {}
+    def _parser(self, args):
+        """ parser method """
+        Ndict = {}
         for arg in args:
             if "=" in arg:
                 kvp = arg.split('=', 1)
@@ -131,17 +131,17 @@ class HBNBCommand(cmd.Cmd):
                             value = float(value)
                         except:
                             continue
-                new_dict[key] = value
-        return new_dict
+                Ndict[key] = value
+        return Ndict
 
     def do_create(self, arg):
-        """Creates a new instance of a class"""
+        """ Create an object of any class """
         args = arg.split()
         if len(args) == 0:
             print("** class name missing **")
             return False
         if args[0] in self.classes:
-            new_dict = self._key_value_parser(args[1:])
+            new_dict = self._parser(args[1:])
             instance = self.classes[args[0]](**new_dict)
         else:
             print("** class doesn't exist **")
