a24791e567c8a08bdaeab6ec26edb1b4c945f325
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-17 19:43:11 +0200

    update tests file

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A unit test module for the console (command interpreter).
+"""
+A unit test module for the console (command interpreter).
 """
 import json
 import MySQLdb
@@ -8,13 +9,11 @@ import sqlalchemy
 import unittest
 from io import StringIO
 from unittest.mock import patch
-
 from console import HBNBCommand
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.city import City
-
 
 
 class TestHBNBCommand(unittest.TestCase):
@@ -32,18 +31,16 @@ class TestHBNBCommand(unittest.TestCase):
         cons.onecmd('create City name="Texas"')
         count_1 = len(storage.all(City).keys())
         self.assertEqual(count_1, count + 1)
-        
-        #second test case
+        # second test case
         count_2 = len(storage.all(City).keys())
         cons.onecmd('create City name="NewYork"')
         count_3 = len(storage.all(City).keys())
         self.assertEqual(count_3, count_2 + 1)
-        #third test case
+        # third test case
         counter = 0
         element_1 = None
-        for element,value in storage.all(City).items():
+        for element, value in storage.all(City).items():
             counter = counter + 1
             if counter == count_3:
                 element_1 = value
         self.assertEqual('NewYork', element_1.__dict__["name"])
-
