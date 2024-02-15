73f1f9b9991ac54f7bc8a2b1c5b6463d9b3fd5d2
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-02-15 18:56:45 +0200

    02-15 18:56

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+from flask import Flask
+" script that starts a Flask web application "
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    "prints Hello HBNB on the main route"
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    "display “HBNB”"
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def parse_C(text):
+    "display “C ” followed by the value of the text variable"
+    new_str = text.replace('_', ' ')
+    return "C " + new_str
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def parse_python(text="is_cool"):
+    "display “Python ”, followed by the value of the text"
+    new_str = text.replace('_', ' ')
+    return "Python " + new_str
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
